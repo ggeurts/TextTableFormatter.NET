1b1821121f08410c915686d8b3fc0bd6ffbd6203
Gerke Geurts <ggeurts@geurtsrus.com>
2018-02-13 22:14:40 +0100

    Added license reference and attribution of code origins

--- TextTableFormatter/Row.cs
+++ TextTableFormatter/Row.cs
@@ -1,3 +1,18 @@
+#region License and attribution
+/*
+ * License: Apache License Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ *
+ * This code is a fork by Gerke Geurts (ggeurts) of TextTableFormatter.NET 1.0.1, a port by
+ * Dario Santarelli (dsantarelli) of the Java TextTableFormatter library.
+ *
+ * Compared to TextTableFormatter 1.0.1, the API has been changed significantly: various style
+ * types have been named differently, columns and column widths are configured differently,
+ * XML escaping and ANSI escape sequences are no longer supported. This library supports
+ * cell content with line breaks as well as wrapping of cell content that is longer than
+ * the column width.
+ */
+#endregion
+
 namespace TextTableFormatter
 {
     using System.Collections.Generic;
